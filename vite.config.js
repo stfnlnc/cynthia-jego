f66e1333a9f1be77d5d6c8483e261411173e349e
Stefan Lancelot <hello@stefanlancelot.com>
2026-03-31 23:05:02 +0200

    add info.js

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/js/lab.js",
+                "resources/js/info.js",
             ],
             refresh: true,
         }),
